dd7aa7c8a0ee216bb83733eb517004cdbd4d2d64
Ritesh Agarwal <ritesh.agarwal@pubmatic.com>
2023-05-23 16:12:18 +0530

    semaphore complete

--- c_app/main.c
+++ c_app/main.c
@@ -3,12 +3,33 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/shm.h> 
+#include <sys/shm.h>
+
+
+
+
+ #include <fcntl.h>
+ #include <sys/stat.h>
+
+ #include <semaphore.h>
+ #include <time.h>
+// sem_t* Go_sem_open(const char *name, int oflag, mode_t mode, unsigned int value)
+// {
+//      return sem_open(name, oflag, mode, value);
+// }
+// #endif
 
 void error (char *msg);
 
 int main (int argc, char **argv)
 {
+	sem_unlink("/testsem");
+	sem_t *sem = sem_open("/testsem",
+						 O_CREAT,
+						 0644,
+						 0);
+	printf("Waiting\n");
+	sem_wait(sem);
     key_t s_key = 1234; 
     int shm_id;
     if((shm_id = shmget(s_key,
@@ -24,6 +45,7 @@ int main (int argc, char **argv)
         error ("shmat");
 	}
 	printf("%s\n",shared_mem_ptr);
+	sem_close(sem);
 }
 
 // Print system error and exit
--- go_app/main.go
+++ go_app/main.go
@@ -29,6 +29,7 @@ func main() {
     // Write a string to the shared memory segment
     message := "Hello, World! from Golang"
     copy(data, []byte(message))
+	fmt.Println("wrote to shmem: " + message)
 
     // Detach from the shared memory segment
     err = unix.SysvShmDetach(data)
@@ -36,6 +37,28 @@ func main() {
         fmt.Println("Error detaching from shared memory segment:", err)
         os.Exit(1)
     }
+
+	var sem semaphore.Semaphore
+	if err := sem.Open("/testsem", 0644, 0); err != nil {
+		fmt.Println("Failed to open: %v", err)
+	}
+
+	if err := sem.Post(); err != nil {
+		fmt.Println("Failed to post: %v", err)
+	}
+	fmt.Println("sem_posted");
+
+	if err := sem.Close(); err != nil {
+		fmt.Println("Failed to close: %v", err)
+	}
+
+
+
+/*
+	if err := sem.Unlink(); err != nil {
+		t.Fatalf("Failed to unlink: %v", err)
+	}
+*/
 
     // Display the message read from the shared memory segment
     //fmt.Println("Message read from shared memory segment:", string(data))
